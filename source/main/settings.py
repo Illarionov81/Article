f2b564dbbe8089ed5e3799ceeb655eb9a1a63e2f
alex <nuvorish81@mail.ru>
2020-12-13 14:50:37 +0600

    Добавленны настройки для accounts в settings.py #4

--- source/main/settings.py
+++ source/main/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'accouns',
     'webapp',
 ]
 
@@ -120,6 +121,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+LOGIN_REDIRECT_URL = 'webapp:index'
+LOGOUT_REDIRECT_URL = 'webapp:index'
+LOGIN_URL = 'accounts:login'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'uploads')
 MEDIA_URL = '/uploads/'
