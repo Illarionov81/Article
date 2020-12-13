a2036627e56d7744582e9fb0cd78fcdb78f617a7
alex <nuvorish81@mail.ru>
2020-12-13 15:30:48 +0600

    Написанны модели для Основные сущности и их поля: Category, Article. Модели зарегистрированны в админке, сделанны миграции.

--- source/webapp/admin.py
+++ source/webapp/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
-# Register your models here.
+from webapp.models import Article, Category
+
+
+class ArticleAdmin(admin.ModelAdmin):
+    filter_horizontal = ('category_id',)
+    list_display = ('id', 'title', 'user_id', )
+    list_display_links = ('id', 'title',)
+
+
+admin.site.register(Article, ArticleAdmin)
+admin.site.register(Category)
--- source/webapp/models.py
+++ source/webapp/models.py
@@ -1,3 +1,32 @@
+from django.contrib.auth import get_user_model
 from django.db import models
 
-# Create your models here.
+
+class Article(models.Model):
+    title = models.CharField(max_length=200, verbose_name='Заголовок',)
+    description = models.TextField(max_length=3000, verbose_name='Описание')
+    category_id = models.ManyToManyField('webapp.Category', related_name='articles', blank=True,
+                                         verbose_name='Категория')
+    user_id = models.ForeignKey(get_user_model(), on_delete=models.SET_DEFAULT, default=1,
+                                related_name='articles', verbose_name='Пользователь')
+    image = models.ImageField(null=True, blank=True, upload_to='user_pics', verbose_name='Фото')
+
+    def __str__(self):
+        return "{}. {}".format(self.pk, self.title)
+
+    class Meta:
+        verbose_name = 'Статья'
+        verbose_name_plural = 'Статьи'
+
+
+class Category(models.Model):
+    title = models.CharField(max_length=200, verbose_name='Заголовок',)
+    parent_id = models.OneToOneField('webapp.Category', on_delete=models.PROTECT, null=True, blank=True,
+                                     verbose_name='Основная категория')
+
+    def __str__(self):
+        return "{}. {}".format(self.pk, self.title)
+
+    class Meta:
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
