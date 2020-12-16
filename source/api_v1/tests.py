4d0bb3b8dd4e4ee107d2adbc5d808487b303ef90
alex <nuvorish81@mail.ru>
2020-12-16 22:15:07 +0600

    Сделаны тесты на получение Json данных статьи, главной категории и подкатегории для GET запроса  тикет #15

--- source/api_v1/tests.py
+++ source/api_v1/tests.py
@@ -1,3 +1,58 @@
-from django.test import TestCase
+import json
 
-# Create your tests here.
+from django.core.management import call_command
+from django.http import JsonResponse
+from django.test import TestCase
+from django.urls import reverse
+
+
+URLS = ['api_v1:categories', 'api_v1:main_categories', 'api_v1:articles']
+
+
+class GetJsonData(TestCase):
+    def test_post_not_login_no_pk(self):
+        for url in URLS:
+            response = self.client.get(reverse(url))
+            self.check_redirect(response)
+
+    def check_redirect(self, response):
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(type(response), JsonResponse)
+
+
+class UnLoginGetArticleJsonTestCase(TestCase):
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        call_command('loaddata', 'fixtures/auth.json', verbosity=0)
+        call_command('loaddata', 'fixtures/dump.json', verbosity=0)
+
+    def test_article_success_data(self):
+        url = reverse('api_v1:articles')
+        self.response = self.client.get(url)
+        print(self.response)
+        data = json.loads(self.response.content)
+        self.assertTrue(len(data) >= 1)
+        self.assertIn('pk', data[0])
+        self.assertIn('title', data[0])
+        self.assertIn('description', data[0])
+        self.assertIn('category_id', data[0])
+        self.assertIn('user_id', data[0])
+        self.assertIn('image', data[0])
+
+    def test_get_main_category_json_data(self):
+        url = reverse('api_v1:main_categories')
+        self.response = self.client.get(url)
+        data = json.loads(self.response.content)
+        self.assertTrue(len(data) >= 1)
+        self.assertIn('pk', data[0])
+        self.assertIn('title', data[0])
+
+    def test_get_json_category_success_data(self):
+        url = reverse('api_v1:categories')
+        self.response = self.client.get(url)
+        data = json.loads(self.response.content)
+        self.assertTrue(len(data) >= 1)
+        self.assertIn('pk', data[0])
+        self.assertIn('title', data[0])
+        self.assertIn('parent_id', data[0])
